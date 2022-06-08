0897115d4098677d079944938e2a4e3cc3d88154
Itamar <itamarshmaya@gmail.com>
2022-06-08 20:43:01 +0300

    slider arrow style

--- src/components/CarouselArrows/NextArrow.jsx
+++ src/components/CarouselArrows/NextArrow.jsx
@@ -1,8 +1,9 @@
+import "./Arrow.css";
 const NextArrow = ({ className, style, onClick }) => {
   return (
     <div
       className={className}
-      style={{ ...style, right: "0", zIndex: "2" }}
+      style={{ ...style, right: "0px", zIndex: "2" }}
       onClick={onClick}
     />
   );
